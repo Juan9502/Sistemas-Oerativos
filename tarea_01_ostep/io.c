ada15bfe8cc0b8422020f69192ad912f510c8823
Juan <jglc1995@gmail.com>
2019-06-19 23:52:08 -0500

    subiendo

--- tarea_01_ostep/io.c
+++ tarea_01_ostep/io.c
@@ -10,24 +10,12 @@ int main(){
         return 1;
       }
 
-      char letra;
-
-     int pos;
-
-    // pos = ftell(flow);
-    pos = fseek(flow, -3, SEEK_END);
-
-    if(pos == 0){
-     printf("posicion 1\n");
-    }else{
-      printf("posicion----\n");
-     }
-
-   /*  while(feof(flow) == 0){
+char letra;
+     while(feof(flow) == 0){
 
         letra = fgetc(flow);
         printf("%c", letra);
-          }*/
+          }
 
   fclose(flow);
   printf("\nLECTURA EXITOSA");
