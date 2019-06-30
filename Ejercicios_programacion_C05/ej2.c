1edc335f29875845ee4eafa18aad4069d5568923
Juan <jglc1995@gmail.com>
2019-06-30 01:06:15 -0500

    Ejercicio 2

--- Ejercicios_programacion_C05/ej2.c
+++ Ejercicios_programacion_C05/ej2.c
@@ -9,10 +9,9 @@
 int main (){
 
    printf("hello world (pid:%d)\n", (int) getpid());
+   // con la funcion open se abre el archivo y lo guardo en una variable llamada text
+   int text =  open("file.txt", O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
    int rc = fork();
-
-   // con la funcion open se abre el archivo y lo guardo en una variable que se llama text
-   int text = open("file.txt", O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
 
    if(rc < 0){
      fprintf(stderr, "fork failed\n");
@@ -24,8 +23,8 @@ int main (){
    write(text, mensaje, strlen(mensaje)); // escribir el mensaje en el archivo
    }
    else{
-    int rc_wait = wait(NULL);
-    printf("hola, yo soy el padre de: %d (rc_wait:%d) (pid:%d)\n", rc, rc_wait, (int) getpid());
+//    int rc_wait = wait(NULL);
+    printf("hola, yo soy el padre de: %d (pid:%d)\n", rc,(int) getpid());
     const char * mensajep = "hola, yo soy el padre\n"; // mensaje que escribira el proceso padre
     write(text, mensajep, strlen(mensajep)); // escribir el mensaje
    }
